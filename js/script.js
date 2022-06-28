204dc7ff3fcfb85999056506f7102aa95b868fe4
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-06-28 17:09:07 +0300

    try to auto detect dark mode

--- js/script.js
+++ js/script.js
@@ -4,43 +4,60 @@ let body_page = document.querySelector('body');
 let card = document.querySelectorAll(".cards__item");
 let textTheme = document.querySelectorAll(".dark-theme-text");
 
+window.matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', event => {
+        if (event.matches) {
+            toDark()
+        } else {
+            toWhite()
+        }
+    })
+
+let toWhite = function () {
+    toggle_ball.classList.remove('toggle__ball_dark');
+    toggle_ball.classList.add('toggle__ball_white');
+    toggle.classList.add('white-theme-toggle');
+    toggle.classList.remove('black-theme-toggle');
+    body_page.classList.add('white-theme-bg');
+    body_page.classList.remove('black-theme-bg');
+
+    for (let i = 0; i < card.length; i++) {
+        card[i].classList.add('white-theme-card-bg');
+        card[i].classList.remove('dark-theme-card-bg');
+    }
+    for (let i = 0; i < textTheme.length; i++) {
+        textTheme[i].classList.add('white-theme-text');
+        textTheme[i].classList.remove('dark-theme-text');
+    }
+}
+
+let toDark = function () {
+    toggle.classList.remove('white-theme-toggle')
+    toggle.classList.add('dark-theme-toggle')
+    toggle_ball.classList.remove('toggle__ball_white');
+    toggle_ball.classList.add('toggle__ball_dark');
+    body_page.classList.add('black-theme-bg');
+    body_page.classList.remove('white-theme-bg');
+    for (let i = 0; i < card.length; i++) {
+        card[i].classList.add('dark-theme-card-bg');
+        card[i].classList.remove('white-theme-card-bg');
+    }
+    for (let i = 0; i < textTheme.length; i++) {
+        textTheme[i].classList.add('dark-theme-text');
+        textTheme[i].classList.remove('white-theme-text');
+    }
+}
+
 
 toggle.addEventListener("click", function () {
     if (toggle_ball.classList.contains('toggle__ball_dark')) {
         // remove black, Add white 
-        toggle_ball.classList.remove('toggle__ball_dark');
-        toggle_ball.classList.add('toggle__ball_white');
-        toggle.classList.add('white-theme-toggle');
-        toggle.classList.remove('black-theme-toggle');
-        body_page.classList.add('white-theme-bg');
-        body_page.classList.remove('black-theme-bg');
-
-        for (let i = 0; i < card.length; i++) {
-            card[i].classList.add('white-theme-card-bg');
-            card[i].classList.remove('dark-theme-card-bg');
-        }
-        for (let i = 0; i < textTheme.length; i++) {
-            textTheme[i].classList.add('white-theme-text');
-            textTheme[i].classList.remove('dark-theme-text');
-        }
+        toWhite()
 
 
     } else {
         // remove white, Add black
+        toDark()
 
-        toggle.classList.remove('white-theme-toggle')
-        toggle.classList.add('dark-theme-toggle')
-        toggle_ball.classList.remove('toggle__ball_white');
-        toggle_ball.classList.add('toggle__ball_dark');
-        body_page.classList.add('black-theme-bg');
-        body_page.classList.remove('white-theme-bg');
-        for (let i = 0; i < card.length; i++) {
-            card[i].classList.add('dark-theme-card-bg');
-            card[i].classList.remove('white-theme-card-bg');
-        }
-        for (let i = 0; i < textTheme.length; i++) {
-            textTheme[i].classList.add('dark-theme-text');
-            textTheme[i].classList.remove('white-theme-text');
-        }
     }
 });
